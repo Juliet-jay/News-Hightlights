820a579d02988dbaa4b1b5cd9c45cc4938aeac60
kathure <juliekmuthee@gmail.com>
2019-07-28 19:05:36 +0300

    add Production Configuration child class

--- app/config.py
+++ app/config.py
@@ -6,3 +6,19 @@ class Config:
     NEWS_SOURCES_BASE_URL='https://newsapi.org/v2/sources?category={}&apiKey={}'
     NEWS_HEADLINES_BASE_URL='https://newsapi.org/v2/top-headlines?sources{}&apiKey={}'
     NEW_ARTICLES_BASE_URL='https://newsapi.org/v2/everything?sources={}&apiKey={}'
+
+class ProdConfig(Config):
+    '''
+    Production Configuration child class
+    Args:
+        Config:The parent configuration class with General configuration settings
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development Configuration child class
+    Args:
+        Config:The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
