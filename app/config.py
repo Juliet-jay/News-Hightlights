e3b48e1b7c061bb4921aec1c50eb8b1d027033a1
kathure <juliekmuthee@gmail.com>
2019-07-28 13:11:59 +0300

    We then store the news base URL inside the app/config file.

--- app/config.py
+++ app/config.py
@@ -2,3 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
+    
+    NEWS_SOURCES_BASE_URL='https://newsapi.org/v2/sources?category={}&apiKey={}'
+    NEWS_HEADLINES_BASE_URL='https://newsapi.org/v2/top-headlines?sources{}&apiKey={}'
+    NEW_ARTICLES_BASE_URL='https://newsapi.org/v2/everything?sources={}&apiKey={}'
