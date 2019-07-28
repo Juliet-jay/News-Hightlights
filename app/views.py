5a46eb973e567778e8918eec215e9af2ede12d82
kathure <juliekmuthee@gmail.com>
2019-07-28 13:26:02 +0300

    import the app instance and from it get the API key from the config object.

--- app/views.py
+++ app/views.py
@@ -1,11 +1,14 @@
 from flask import render_template
 from app import app
 
-# Views
-@app.route('/')
+#Views
+@main.route ('/')
 def index():
+    '''
+    Returns index page and its data
+    '''
 
-    '''
-    View root page function that returns the index page and its data
-    '''
-    return render_template('index.html')
+    #Getting headlines
+    news_headlines = get_headlines()
+    title = 'News Highlight- Fast and reliable way to get news'
+    return render_template('index.html', title = title, headlines = news_headlines)
